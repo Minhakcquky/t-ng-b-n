06ee9ca018942a90a142aa0b0bacaa31f8620f48
Minhakcquky <88574918+Minhakcquky@users.noreply.github.com>
2021-08-07 15:17:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,15 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Bông à!',
+    introDesc: `Hãy trả lời những câu hỏi sau một cách chân thật nhất nhé`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: 'Cậu yêu tớ có phải không nào ?',
+    desc: 'Nếu không trả lời mà thoát ra thì là vợ tui nha !!!',
+    btnYes: 'Ư! đúng rùi ó',
+    btnNo: 'Không! cậu mơ à',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại vì Minh đẹp trai vcl<3333333333',
+    mess: 'Tui biết mà 🥰. Tui cũng yêu bạn nhiều lắm 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
